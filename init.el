54517cc4b9d4efde79e2e71c3227c5af42a6b513
Wojciech Kozlowski <wk@wojciechkozlowski.eu>
2017-08-17 20:37:33 +0100

    Add header to init file

---
+++ init.el
@@ -0,0 +1,8 @@
+;;; init.el --- Emacs Initialization File
+;;
+;; Copyright (c) 2017 Wojciech Kozlowski
+;;
+;; Author: Wojciech Kozlowski <wojciech.kozlowski@vivaldi.net>
+;; URL: https://gitlab.wojciechkozlowski.eu/config/emacs.d
+;;
+;;; License: GPLv3
