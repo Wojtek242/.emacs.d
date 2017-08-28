519634beba38e1bcb6c1108dd1fed50482f75b3f
Wojciech Kozlowski <wk@wojciechkozlowski.eu>
2017-08-28 18:44:38 +0100

    Fix smartparens keybindings

--- modules/parentheses.el
+++ modules/parentheses.el
@@ -57,13 +57,15 @@
     (define-key smartparens-mode-map (kbd "C-M-f") 'sp-forward-sexp)
     (define-key smartparens-mode-map (kbd "C-M-b") 'sp-backward-sexp)
 
-    (define-key smartparens-mode-map (kbd "C-M-d") 'sp-down-sexp)
-    (define-key smartparens-mode-map (kbd "C-M-a") 'sp-backward-down-sexp)
-    (define-key smartparens-mode-map (kbd "C-S-d") 'sp-beginning-of-sexp)
-    (define-key smartparens-mode-map (kbd "C-S-a") 'sp-end-of-sexp)
-
-    (define-key smartparens-mode-map (kbd "C-M-e") 'sp-up-sexp)
     (define-key smartparens-mode-map (kbd "C-M-u") 'sp-backward-up-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-S-u") 'sp-up-sexp)
+
+    (define-key smartparens-mode-map (kbd "C-M-d") 'sp-down-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-S-d") 'sp-backward-down-sexp)
+
+    (define-key smartparens-mode-map (kbd "C-S-a") 'sp-beginning-of-sexp)
+    (define-key smartparens-mode-map (kbd "C-S-e") 'sp-end-of-sexp)
+
     (define-key smartparens-mode-map (kbd "C-M-t") 'sp-transpose-sexp)
 
     (define-key smartparens-mode-map (kbd "C-M-n") 'sp-next-sexp)
@@ -75,8 +77,8 @@
     (define-key smartparens-mode-map (kbd "C-<right>") 'sp-forward-slurp-sexp)
     (define-key smartparens-mode-map (kbd "C-<left>") 'sp-forward-barf-sexp)
 
-    (define-key smartparens-mode-map (kbd "M-D") 'sp-splice-sexp)
-    (define-key smartparens-mode-map (kbd "C-M-D") 'sp-splice-sexp-killing-forward)
+    (define-key smartparens-mode-map (kbd "M-S-d") 'sp-splice-sexp)
+    (define-key smartparens-mode-map (kbd "C-S-d") 'sp-splice-sexp-killing-forward)
     (define-key smartparens-mode-map (kbd "C-M-<backspace>") 'sp-splice-sexp-killing-backward)
     (define-key smartparens-mode-map (kbd "C-S-<backspace>") 'sp-splice-sexp-killing-around)
 
