fb01451ce32854918428a6af60ee3b0028d207c3
Wojciech Kozlowski <wk@wojciechkozlowski.eu>
2017-09-02 21:24:18 +0100

    Refresh diff-hl after Magit commits

--- modules/version-control.el
+++ modules/version-control.el
@@ -61,7 +61,8 @@
   (use-package diff-hl
     :init
     (global-diff-hl-mode)
-    (add-hook 'dired-mode-hook 'diff-hl-dired-mode))
+    (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
+    (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
 
   ;; --------------------------------------------------------------------------
   ;; Diff mode settings.
