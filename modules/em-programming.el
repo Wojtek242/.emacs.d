051f3007bb59e8b1d585a64acb1da517fe3ea8ea
Wojciech Kozlowski <wk@wojciechkozlowski.eu>
2019-03-16 23:45:11 +0100

    Disable lsp-ui-doc

--- modules/em-programming.el
+++ modules/em-programming.el
@@ -66,6 +66,7 @@
   (use-package lsp-ui
     :commands lsp-ui-mode
     :config
+    (setq lsp-ui-doc-enable nil)
     (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
     (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
 
